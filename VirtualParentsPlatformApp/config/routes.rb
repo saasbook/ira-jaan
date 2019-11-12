50e7f4222fdd557ed2dbd12043ebb42e1fae8312
Shawn Magee <shawnmagee@berkeley.edu>
2019-11-11 19:44:55 -0800

    added react and redux

--- VirtualParentsPlatformApp/config/routes.rb
+++ VirtualParentsPlatformApp/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'hello_world', to: 'hello_world#index'
+  get 'login/index'
+  get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
 end
