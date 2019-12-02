80c8bfa6fe0722711f5389ef38db16a7d9b0daed
Stephen Zhou <stephenzhou@berkeley.edu>
2019-12-01 21:49:06 -0800

    merge over routes.rb

--- VirtualParentsPlatformApp/config/routes.rb
+++ VirtualParentsPlatformApp/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # devise_for :administrators
   # devise_for :children
   get 'login/index'
+  post 'login/administrator'
+  post 'login/child'
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
@@ -10,6 +12,7 @@ Rails.application.routes.draw do
   resources :administrators do
       resources :activities, :rewards
   end
+  get 'administrators/:id/connect', to: 'administrators#connect', as: :connect_admin
   get 'administrators/:id/points', to: 'administrators#add_points', as: :admin_points
   put 'administrators/:id/points', to: 'administrators#update_points'
 
@@ -17,6 +20,7 @@ Rails.application.routes.draw do
   resources :children do
       resources :activities, :rewards
   end
+  get 'children/:id/connect', to: 'children#connect', as: :connect_child
 
   post 'child/create'
   get 'tasks/index'
