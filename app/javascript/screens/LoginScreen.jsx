53b4abbb8aa2e47bf70ac73659512c9a767611e4
brian fu <brianfu9@gmail.com>
2019-12-05 03:55:56 -0800

    added descriptor to login page

--- app/javascript/screens/LoginScreen.jsx
+++ app/javascript/screens/LoginScreen.jsx
@@ -19,10 +19,24 @@ const styles = {
   container: {
     backgroundColor: "white",
     display: "flex",
-    flexDirection: "column",
-    flex: 1,
+    // flexDirection: "column",
+    // flex: 1,
     height: "100vh",
     margin: 0,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  container_left: {
+    float: "left",
+    width: "49%",
+    marginLeft: "8%",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  container_right: {
+    float: "left",
+    width: "49%",
+    marginLeft: "8%",
     alignItems: "center",
     justifyContent: "center"
   },
@@ -75,6 +89,22 @@ const styles = {
     marginTop: 20,
     fontFamily: "Helvetica",
     fontWeight: "800"
+  },
+  textSubtext: {
+    color: "#6A6A6A",
+    fontSize: 24,
+    margin: 0,
+    marginTop: 20,
+    fontFamily: "Helvetica",
+    fontWeight: "1000"
+  },
+  textParagraph: {
+    color: "#6A6A6A",
+    fontSize: 18,
+    margin: 0,
+    marginTop: 20,
+    fontFamily: "Helvetica",
+    // fontWeight: "800"
   }
 };
 
@@ -122,42 +152,48 @@ class LoginScreen extends Component {
   render() {
     return (
       <div style={styles.container}>
-        <p style={styles.title}>Sign In</p>
-        <div style={styles.textInputContainer}>
-          <p style={styles.textInputLabel}>USERNAME</p>
-          <input
-            type="text"
-            value={this.state.username}
-            onChange={this.onChangeUsername}
-            style={styles.textInput}
-          />
-          <p style={styles.textInputLabel}>PASSWORD</p>
-          <input
-            type="text"
-            value={this.state.password}
-            onChange={this.onChangePassword}
-            style={styles.textInput}
-          />
+        <div style={styles.container_left}>
+          <p style={styles.title}>Ira Jaan Foundation</p>
+          <p style={styles.textSubtext}>Empowering communities for a brighter future</p>
         </div>
-        <button style={styles.button} onClick={this.handleLogIn}>
-          <p style={styles.buttonLabel}>Sign In</p>
-        </button>
-        <Link to="/signup" style={styles.textInputLabel}>
-          Don't have an account? Sign up
+        <div style={styles.container_right}>
+          <p style={styles.title}>Sign In</p>
+          <div style={styles.textInputContainer}>
+            <p style={styles.textInputLabel}>USERNAME</p>
+            <input
+              type="text"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+              style={styles.textInput}
+            />
+            <p style={styles.textInputLabel}>PASSWORD</p>
+            <input
+              type="text"
+              value={this.state.password}
+              onChange={this.onChangePassword}
+              style={styles.textInput}
+            />
+          </div>
+          <button style={styles.button} onClick={this.handleLogIn}>
+            <p style={styles.buttonLabel}>Sign In</p>
+          </button>
+          <Link to="/signup" style={styles.textInputLabel}>
+            <p>Don't have an account? Sign up</p>
         </Link>
+        </div>
       </div>
-    );
-  }
-}
-
-// Redux Container that passes in redux state
-const Container = connect(
+        );
+      }
+    }
+    
+    // Redux Container that passes in redux state
+    const Container = connect(
   createStructuredSelector({
-    user: state => state.user,
-    stoof: state => state.stoof
-  }),
-  { setUser }
-)(LoginScreen);
-
-// We use the container in other files like a UI component
-export default Container;
+          user: state => state.user,
+        stoof: state => state.stoof
+      }),
+  {setUser}
+        )(LoginScreen);
+        
+        // We use the container in other files like a UI component
+        export default Container;
